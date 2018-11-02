c4dae407293b9b59da089233e7eb9ccc1e54cbe2
huy9997 <huy9997@gmail.com>
2018-11-02 15:28:40 -0700

    added instructions to the readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SMSPythonBot
+-make sure flask is installed 
+-make sure your using python3 
+-install ngrok 
+-be a collaborator on twilio s
+
+to run the program 
+run python3 SMSBot.py 
+run ngrok 
+copy the http ngrok link to twilio 
+
--- SMSBot.py
+++ SMSBot.py
@@ -14,24 +14,22 @@ auth_token = config.auth_token
 #rest client
 client = Client(account_sid, auth_token)
 
-# gather user input: 
-# phone numbers and message
-
-#userInputPhoneNumber = input("What phone number would you like to contact")
-#userInputMessage = input("What message would you like to send?")
-
-#send message
-"""
-messagesSend = client.messages \
-  .create(
-  body = userInputMessage,
-  from_ = '+15109014776',
-  to = '+19258951162'
-  )
-"""
-#respond to
-   
 app = Flask(__name__)
+
+#userPrompt = input("What would you like to do today? Send, CheckMessages")
+  
+  #if userPrompt=="send" or userPrompt=="Send" :
+    #userInputPhoneNumber = input("What phone number would you like to contact")
+userInputMessage = input("What message would you like to send?") 
+    #send message
+messagesSend = client.messages \
+    .create(
+    body = userInputMessage,
+    from_ = '+15109014776',
+    to = '+19258951162'
+    )
+#respond
+
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
@@ -47,7 +45,7 @@ def incoming_sms():
     elif body == 'Huy':
         resp.message("huy is way better than alvin ")
     else:
-      resp.message("none of the above")
+        resp.message("none of the above")
 
     return str(resp)
 
@@ -59,5 +57,25 @@ if __name__ == "__main__":
   #
   
 """
-The 
+Flow of application 
+
+  ADMIN
+  pick who to contact
+  write what message to send them 
+
+  USER
+  recieve message
+  respond to ADMIN
+  parse through the response for a yes no yeah.
+    yes or no  
+      if yes 
+        thank you 
+      if no 
+        ask why 
+    maybe or unsure 
+      forward message to huys phone 
+  HUY 
+    recieve unsure message from ADMIN
+    respond to message send to ADMIN. Then ADMIN send to USER
+  
 """
