9eb4a7f89d2735e120c887fa93758433d387f003
Huy Nguyen <huy9997@gmail.com>
2018-11-03 22:01:36 -0700

    got the sms response working again

--- SMSBot.py
+++ SMSBot.py
@@ -8,53 +8,28 @@ from flask import Flask, request
 account_sid = config.account_sid
 auth_token = config.auth_token
 client = Client(account_sid, auth_token) #rest client
+
 app = Flask(__name__) # start a Flask application 
 
 
-def menu():
-    userInputMenu = input("What would you like to do today: sendSMS, viewSMS, editContact")
-    if(userInputMenu == "sendSMS"):
-        userInputPhoneNumber = input ("What phone number would you like to send this message to")
-        userInputMessage = input("What message would you like to send?") 
-        listContacts()
-        messagesSending()
-    if(userInputMenu == "viewSMS"):
-        print("viewSMS is not avaiable at this time. :(")
-        viewSMS()
+userInputMenu = "sendSMS"
+    #userInputMenu = input("What would you like to do today: sendSMS, viewSMS, editContact\n")
+if(userInputMenu == "sendSMS"):
+    userInputPhoneNumber = input ("What phone number would you like to send this message to \n")
+    userInputMessage = input("What message would you like to send?\n") 
+if(userInputMenu == "viewSMS"):
+    print("viewSMS is not avaiable at this time. :(")
     
      # user input in the textmessage
-
-"""
-def saveContact():
-    save the contacts in dictionary Key= name Value = phone number
-
-def editContact():
-    # to go back and edit the phone number / Name  of saveContact 
-    # or to delete a contact 
-
-def listContacts():
-    # list all the keys in the dict 
-    # user types in the contacts he would like to 
-
-
-def messagesSending():
-    #call the sendSMS function to send a message to all the messages you would like to send to. 
-
-
-def viewSMS():
-    # have the admin view all conversations made by the bot
-
-"""
-
 #send SMS message 
-def sendSMS():
+def sendSMSMessage():
     messagesSend = client.messages \
     .create(
     body = userInputMessage,
     from_ = '+15109014776',
-    to = userInputMessage
+    to = userInputPhoneNumber
     )
-
+sendSMSMessage()
 #response text message 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
@@ -84,3 +59,27 @@ if __name__ == "__main__":
   #To Do List: 
   # work on getting the 
   #
+
+
+
+"""
+def saveContact():
+    save the contacts in dictionary Key= name Value = phone number
+
+def editContact():
+    # to go back and edit the phone number / Name  of saveContact 
+    # or to delete a contact 
+
+def listContacts():
+    # list all the keys in the dict 
+    # user types in the contacts he would like to 
+
+
+def messagesSending():
+    #call the sendSMS function to send a message to all the messages you would like to send to. 
+
+
+def viewSMS():
+    # have the admin view all conversations made by the bot
+
+"""
