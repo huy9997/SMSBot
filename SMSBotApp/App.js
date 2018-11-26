948ee76770908a6c17f055cd122b1557d0d04376
niszeto <niszeto@gmail.com>
2018-11-25 20:13:37 -0800

    contact list can call an api and display several contacts in a scroll view

--- SMSBotApp/App.js
+++ SMSBotApp/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
 import StartScreen from "./screens/Start";
 import CreateScreen from "./screens/CreateEvent";
-import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 export default class App extends React.Component {
   render() {
@@ -10,19 +10,7 @@ export default class App extends React.Component {
       <View style={styles.mainContainer}>
         {/* <StartScreen />
       <CreateScreen /> */}
-
-        <ScrollView>
-          <Contact phoneNumber="(415) 123-1234" name="nick" />
-          <Contact phoneNumber="(415) 123-1234" name="nick" />
-          <Contact phoneNumber="(415) 123-1234" name="nick" />
-          <Contact phoneNumber="(415) 123-1234" name="nick" />
-          <Contact phoneNumber="(415) 123-1234" name="nick" />
-          <Contact phoneNumber="(415) 123-1234" name="nick" />
-          <Contact phoneNumber="(415) 123-1234" name="nick" />
-          <Contact phoneNumber="(415) 123-1234" name="nick" />
-          <Contact phoneNumber="(415) 123-1234" name="nick" />
-          <Contact phoneNumber="(415) 123-1234" name="nick" />
-        </ScrollView>
+        <ContactList />
       </View>
     );
   }
---
+++ SMSBotApp/components/ContactList.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
+import Contact from "./Contact";
+
+class ContactList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    contacts: [],
+    isLoading: true
+  };
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    const responseJson = await response.json();
+    this.setState({ contacts: responseJson, isLoading: false });
+  }
+
+  render() {
+    const { contacts, isLoading } = this.state;
+
+    return (
+      <ScrollView>
+        {isLoading ? (
+          <Contact phoneNumber="(415) 123-1234" name="nick" />
+        ) : (
+          <React.Fragment>
+            {contacts.map(contact => {
+              return (
+                <Contact
+                  key={contact.id}
+                  name={contact.name}
+                  phoneNumber={contact.phone}
+                />
+              );
+            })}
+          </React.Fragment>
+        )}
+      </ScrollView>
+    );
+  }
+}
+
+export default ContactList;
