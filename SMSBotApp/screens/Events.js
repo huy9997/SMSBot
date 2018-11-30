cfc790cf9bce08613d243d5ce1c80e622bae28db
niszeto <niszeto@gmail.com>
2018-11-29 20:24:32 -0800

    added left and right header icons to event screen with working onPress with state changes

--- SMSBotApp/screens/Events.js
+++ SMSBotApp/screens/Events.js
@@ -1,13 +1,54 @@
 import React from "react";
 
-import { StyleSheet, View } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 import EventsList from "../components/EventList";
 import IconButton from "../components/IconButton";
 
+// settings option ?
+// show search bar and filter from search bar input ?
+
 class Events extends React.Component {
-  static navigationOptions = {
-    title: "Events"
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isSearching: false
+    };
+  }
+
+  componentDidMount() {
+    this.props.navigation.setParams({ isSearching: this.isSearching });
+  }
+
+  isSearching = () => {
+    const { isSearching } = this.state;
+    console.log(isSearching);
+
+    this.setState({ isSearching: !isSearching });
+  };
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerTitle: "Events",
+      headerLeft: (
+        <IconButton
+          borderStyle={{
+            margin: 30
+          }}
+          source={require("../assets/icon.png")}
+        />
+      ),
+      headerRight: (
+        <IconButton
+          borderStyle={{
+            margin: 30
+          }}
+          source={require("../assets/icon.png")}
+          onPress={navigation.getParam("isSearching")}
+        />
+      )
+    };
   };
 
   createEvent = () => {
